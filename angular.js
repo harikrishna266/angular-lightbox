64f2660a344fb763f26f03b4eb63a9eec9fd20ce
harikrishna266 <harikrishna266@gmail.com>
2014-05-05 18:28:21 +0530

    fixed the width for thumpbnail

--- angular.js
+++ angular.js
@@ -2,12 +2,16 @@ var app = angular.module('app', []);
 app.controller('lighbox', ['$scope', function($scope){
 	$scope.groups = [
 			    {
-			      thumb: 'http://images.gizmag.com/gallery_tn/ge_healthcare_imaging_competition_2012-1.jpg',
-			      image: 'http://images.gizmag.com/gallery_tn/ge_healthcare_imaging_competition_2012-1.jpg'
+			      thumb: 'http://mcgovern.mit.edu/news/wp-content/uploads/2013/08/image7LR.jpg',
+			      image: 'http://mcgovern.mit.edu/news/wp-content/uploads/2013/08/image7LR.jpg'
 			    },
 			    {
-			      thumb: 'http://a1.mzstatic.com/us/r30/Purple2/v4/86/f9/b2/86f9b269-1757-bd4c-d0ce-f38dcd8ff785/mzl.zldmcswx.100x100-75.jpg',
-			      image: 'http://a1.mzstatic.com/us/r30/Purple2/v4/86/f9/b2/86f9b269-1757-bd4c-d0ce-f38dcd8ff785/mzl.zldmcswx.100x100-75.jpg'
+			      thumb: 'http://icdn4.digitaltrends.com/image/microsoft_xp_bliss_desktop_image-650x0.jpg',
+			      image: 'http://icdn4.digitaltrends.com/image/microsoft_xp_bliss_desktop_image-650x0.jpg'
+			    },
+			    {
+			      thumb: 'http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg',
+			      image: 'http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg'
 			    }
 			  ];
 
@@ -32,19 +36,18 @@ app.directive('lightbox', function() {
       scope:true,
       require:'modal',
       controller:function($scope){
-									
-      },
+	  },
       link:function(scope, element, attrs,modalCtrl){
       	scope.showLight = false;
       	scope.showpopup = function(imagenew){
       			scope.showlight = 'show';
       			scope.pop = imagenew;
       	}
-
-      	
-   	  },
-      template:"<div ><li ng-repeat=\"group in groups\" ><span ng-click=\"showpopup(group.thumb)\"><img src='{{group.thumb}}' ng-click=\"showpopup('{{group.thumb}}')\" /></span></li><div class=\"lighbox\" ng-show=showlight=='show'>{{showLight}}<img src='{{pop}}'></div></div>"
-
+      	scope.removelightbox = function(){
+      		scope.showlight = 'FALSE';
+      	}
+	  },
+      template:"<div ><li ng-repeat=\"group in groups\" ><span ng-click=\"showpopup(group.thumb)\"><img width=\"100px\" height=\"100px\" src='{{group.thumb}}' ng-click=\"showpopup('{{group.thumb}}')\" /></span></li><div class=\"black_overlay\" ng-click=\"removelightbox()\" ng-show=showlight=='show'><div class=\"lighbox\"><img src='{{pop}}'></div></div></div>"
 
   };
 });

1da779bd44500c5d7358112f25ff692754c5a385
harikrishna266 <harikrishna266@gmail.com>
2014-05-07 13:11:19 +0530

    loghtbox basic version

--- angular.js
+++ angular.js
@@ -1,19 +1,19 @@
 var app = angular.module('app', []);
 app.controller('lighbox', ['$scope', function($scope){
-	$scope.groups = [
-			    {
-			      thumb: 'http://mcgovern.mit.edu/news/wp-content/uploads/2013/08/image7LR.jpg',
-			      image: 'http://mcgovern.mit.edu/news/wp-content/uploads/2013/08/image7LR.jpg'
-			    },
-			    {
-			      thumb: 'http://icdn4.digitaltrends.com/image/microsoft_xp_bliss_desktop_image-650x0.jpg',
-			      image: 'http://icdn4.digitaltrends.com/image/microsoft_xp_bliss_desktop_image-650x0.jpg'
-			    },
-			    {
-			      thumb: 'http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg',
-			      image: 'http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg'
-			    }
-			  ];
+	$scope.groups = {
+					    1:{
+					      thumb: 'http://www.altiusdirectory.com/Travel/images/McKinley%20Mountain%20Image.jpg',
+					      image: 'http://www.altiusdirectory.com/Travel/images/McKinley%20Mountain%20Image.jpg'
+					    },
+					    2:{
+					      thumb: 'https://lh4.ggpht.com/zQIXMNP87brRkMSRRiALQkgF-JRQeBW5vMgqwUt3xMwKw3yeZeZyH1GU6lzXNbDBuRM=w300',
+					      image: 'https://lh4.ggpht.com/zQIXMNP87brRkMSRRiALQkgF-JRQeBW5vMgqwUt3xMwKw3yeZeZyH1GU6lzXNbDBuRM=w300'
+					    },
+					    3:{
+					      thumb: 'http://ecx.images-amazon.com/images/I/81UUXP-xN4L._SL500_AA300_.png',
+					      image: 'http://ecx.images-amazon.com/images/I/81UUXP-xN4L._SL500_AA300_.png'
+					    }
+			  };
 
 }])
 
@@ -34,20 +34,26 @@ app.directive('lightbox', function() {
   return {
       restrict: 'E',
       scope:true,
-      require:'modal',
       controller:function($scope){
 	  },
-      link:function(scope, element, attrs,modalCtrl){
+      link:function(scope, element, attrs){
       	scope.showLight = false;
       	scope.showpopup = function(imagenew){
-      			scope.showlight = 'show';
-      			scope.pop = imagenew;
+      		console.log("here");
+      		console.log(imagenew);
+      		scope.showlight = 'show';
+      		scope.pop = imagenew;
       	}
       	scope.removelightbox = function(){
       		scope.showlight = 'FALSE';
       	}
+      	scope.nextpopup  = function(nextimage){
+  			scope.showlight = 'show';
+  			console.log(nextimage);
+  			scope.pop = nextimage;
+  		}
 	  },
-      template:"<div ><li ng-repeat=\"group in groups\" ><span ng-click=\"showpopup(group.thumb)\"><img width=\"100px\" height=\"100px\" src='{{group.thumb}}' ng-click=\"showpopup('{{group.thumb}}')\" /></span></li><div class=\"black_overlay\" ng-click=\"removelightbox()\" ng-show=showlight=='show'><div class=\"lighbox\"><img src='{{pop}}'></div></div></div>"
+      templateUrl:"lightbox.html"
 
   };
 });

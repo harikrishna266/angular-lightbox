941c4c2d7964463d4181ca9144d4f3aca1906227
harikrishna <harikrishna@bridge>
2014-05-13 18:54:41 +0530

    comiting

--- angular.js
+++ angular.js
@@ -3,15 +3,21 @@ app.controller('lighbox', ['$scope', function($scope){
 	$scope.groups = {
 					    1:{
 					      thumb: 'http://www.altiusdirectory.com/Travel/images/McKinley%20Mountain%20Image.jpg',
-					      image: 'http://www.altiusdirectory.com/Travel/images/McKinley%20Mountain%20Image.jpg'
+					      image: 'http://www.altiusdirectory.com/Travel/images/McKinley%20Mountain%20Image.jpg',
+					      title: 'This is a dummy title for pic 1',
+					      description:'this is where the description goes'
 					    },
 					    2:{
 					      thumb: 'https://lh4.ggpht.com/zQIXMNP87brRkMSRRiALQkgF-JRQeBW5vMgqwUt3xMwKw3yeZeZyH1GU6lzXNbDBuRM=w300',
-					      image: 'https://lh4.ggpht.com/zQIXMNP87brRkMSRRiALQkgF-JRQeBW5vMgqwUt3xMwKw3yeZeZyH1GU6lzXNbDBuRM=w300'
+					      image: 'https://lh4.ggpht.com/zQIXMNP87brRkMSRRiALQkgF-JRQeBW5vMgqwUt3xMwKw3yeZeZyH1GU6lzXNbDBuRM=w300',
+					      title: 'This is a dummy title for pic 2',
+					      description:'this is where the description for pic 2 goes '
 					    },
 					    3:{
 					      thumb: 'http://ecx.images-amazon.com/images/I/81UUXP-xN4L._SL500_AA300_.png',
-					      image: 'http://ecx.images-amazon.com/images/I/81UUXP-xN4L._SL500_AA300_.png'
+					      image: 'http://ecx.images-amazon.com/images/I/81UUXP-xN4L._SL500_AA300_.png',
+					      title: 'This is a dummy title for pic 2',
+					      description:'this is where the description for pic 2 goes '
 					    }
 			  };
 
@@ -33,7 +39,7 @@ app.directive('modal',function(){
 app.directive('lightbox', function() {
   return {
       restrict: 'E',
-      scope:true,
+      scope:false,
       controller:function($scope){
 	  },
       link:function(scope, element, attrs){
@@ -49,7 +55,7 @@ app.directive('lightbox', function() {
       	}
       	scope.nextpopup  = function(nextimage){
   			scope.showlight = 'show';
-  			console.log(nextimage);
+  			console.log(scope.groups);
   			scope.pop = nextimage;
   		}
 	  },
